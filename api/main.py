eb3fe9ed43a620db2cf33a3dab2325a2d3a529d7
Oleh Savchenko <olegqm@gmail.com>
2025-05-01 01:11:25 +0200

    ENV variables added

--- api/main.py
+++ api/main.py
@@ -3,9 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, APIRouter
 from routers import test_endpoint
 
-root_path = os.getenv("FASTAPI_ROOT_PATH", "")
-# app = FastAPI(root_path=root_path)
-
 app = FastAPI(
     docs_url="/api/docs",
     redoc_url="/api/redoc",
