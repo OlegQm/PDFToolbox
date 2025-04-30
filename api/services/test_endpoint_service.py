c1dafe183ab1c4e72fe6104f0e2bb80e1ee34894
Oleh Savchenko <olegqm@gmail.com>
2025-04-30 23:53:24 +0200

    Fastapi design changed

---
+++ api/services/test_endpoint_service.py
@@ -0,0 +1,8 @@
+def get_message():
+    """
+    Returns a message indicating that the endpoint is working.
+    
+    Returns:
+        dict: A JSON response containing a "message" key with the value "pong!".
+    """
+    return {"message": "pong!"}
