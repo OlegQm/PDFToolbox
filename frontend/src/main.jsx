2ce5a07998ccbcdc243bba4d8f7cf463661d981e
Alkotusha Anton <antkolotusha@gmail.com>
2025-05-12 18:58:36 +0200

    Return to usual touter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { HashRouter as RouterProvider } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router.jsx';
 
 const basename = import.meta.env.BASE_URL
 
 createRoot(document.getElementById('root')).render(
-    <RouterProvider basename={basename}>
+    <BrowserRouter basename={basename}>
         <Router />
-    </RouterProvider>
+    </BrowserRouter>
 );
