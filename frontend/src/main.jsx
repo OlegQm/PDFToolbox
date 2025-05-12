47da3a7c554143c7da1ac54809ae7c7b8125e0bd
Alkotusha Anton <antkolotusha@gmail.com>
2025-05-12 18:33:51 +0200

    Router changed on HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { HashRouter as RouterProvider } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router.jsx';
 
 const basename = import.meta.env.BASE_URL
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={basename}>
+    <RouterProvider basename={basename}>
         <Router />
-    </BrowserRouter>
+    </RouterProvider>
 );
