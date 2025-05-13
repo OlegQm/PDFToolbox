deecfc26b0619fd2fdcc74215b56262aac533e08
Alkotusha Anton <antkolotusha@gmail.com>
2025-05-14 01:42:38 +0200

    Locale changed to 'en'

--- api/services/authorization/geoip_service.py
+++ api/services/authorization/geoip_service.py
@@ -1,4 +1,3 @@
-import os
 import geoip2.database
 from pathlib import Path
 from functools import lru_cache
@@ -6,18 +5,42 @@ from typing import Tuple
 
 @lru_cache()
 def get_reader() -> geoip2.database.Reader:
+    """
+    Returns a cached instance of the GeoIP2 database reader.
+
+    This function uses an LRU (Least Recently Used) cache to store and reuse
+    the GeoIP2 database reader instance, which provides efficient access to
+    geolocation data. The database file is expected to be located in the
+    `geoip` directory two levels above the current file's directory, with
+    the filename `GeoLite2-City.mmdb`.
+
+    Returns:
+        geoip2.database.Reader: An instance of the GeoIP2 database reader.
+
+    Raises:
+        FileNotFoundError: If the GeoLite2-City.mmdb file is not found at the
+            expected location.
+    """
     db_path = Path(__file__).resolve().parents[2] / "geoip" / "GeoLite2-City.mmdb"
-    return geoip2.database.Reader(str(db_path))
+    return geoip2.database.Reader(str(db_path), locales=['en'])
 
 async def resolve_geo(ip: str) -> Tuple[str, str]:
     """
-    По IP берёт город и страну из локальной MaxMind GeoLite2 City.
+    Returns a tuple (city_name, country_name) based on the IP.
+    If nothing is found, both values will be "".
     """
     try:
-        reader = get_reader()
-        resp = reader.city(ip)
-        city = resp.city.name or ""
-        country = resp.country.name or ""
-        return resp
-    except Exception as e:
-        return e
+        resp = get_reader().city(ip)
+        city = (
+            resp.city.names.get("en", "Default city")
+            or resp.city.name
+            or "Default city"
+        )
+        country = (
+            resp.country.names.get("en", "Default country")
+            or resp.country.name
+            or "Default country"
+        )
+        return city, country
+    except Exception:
+        return "Default city", "Default country"
